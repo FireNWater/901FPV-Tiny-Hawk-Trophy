52a9bf81e10a0d5e58b459f25257920dabd82463
Keith <FireNWater@users.noreply.github.com>
2021-01-31 16:15:26 -0600

    Create 901FPV_Tiny_Hawk_Web_Page.h
    This is the file to edit to update current and former winners of the 901FPV Tiny Hawk Champiomship Trophy.

---
+++ 901FPV_Tiny_Hawk_Web_Page.h
@@ -0,0 +1,13 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<HTML>
+	<HEAD>
+		<TITLE>901FPV Tiny Hawk Champiom!!</TITLE>
+	</HEAD>
+<BODY>
+	<CENTER>
+        <B>901FPV Tiny Hawk Champiom!!</B>
+		<B>Dream_Z!!!</B>
+	</CENTER>	
+</BODY>
+</HTML>
+)=====";
