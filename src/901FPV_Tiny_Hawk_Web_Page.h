d9b287d6f956b661142eb0da54358aa2658ee3c9
Keith <FireNWater@users.noreply.github.com>
2021-02-01 10:59:26 -0600

    Update 901FPV_Tiny_Hawk_Web_Page.h

--- src/901FPV_Tiny_Hawk_Web_Page.h
+++ src/901FPV_Tiny_Hawk_Web_Page.h
@@ -5,8 +5,13 @@ const char MAIN_page[] PROGMEM = R"=====(
 	</HEAD>
 <BODY>
 	<CENTER>
-        <B>901FPV Tiny Hawk Champiom!!</B> <br>
-		<B>Dream_Z!!!</B>
+		<font size="+3" color="red">
+        		<B>Current 901FPV Tiny Hawk Champiom!!</B> <br>
+			<B>Dream_Z!!! -- 2/13/21</B> <br> <br>
+		<font size="+2" color="black">
+			<B>Former 901FPV Tiny Hawk Champioms!!</B> <br>
+			<B>Dream_Z!!! -- 2/13/21</B> <br>
+		</font>
 	</CENTER>	
 </BODY>
 </HTML>
